8de898eb082b2bdce4640a87ef5269b3923329f4
bestubuntuever <gogebakanoguz4@gmail.com>
2022-10-24 00:58:46 +0300

    Memory feature added.

--- main.py
+++ main.py
@@ -62,39 +62,49 @@ def check_right_child(triangle, parent_row_count, parent_row_idx):
 """
 	
 """
-def calc_max_path_recursive(triangle, parent_row_count, parent_row_idx):
+def calc_max_path_recursive(triangle, memory_dict, node_row_count, node_row_idx):
 
 	left_subtree_max = right_subtree_max = None
+	memory_key = str(node_row_count) + '-' + str(node_row_idx)
 
-	print(parent_row_count, parent_row_idx)
-	if check_left_child(triangle, parent_row_count, parent_row_idx):
-		left_subtree_max = calc_max_path_recursive(triangle, parent_row_count + 1, parent_row_idx)
-	if check_right_child(triangle, parent_row_count, parent_row_idx):
-		right_subtree_max = calc_max_path_recursive(triangle, parent_row_count + 1, parent_row_idx + 1)
+	if memory_key in memory_dict:
+		return memory_dict[memory_key]
 
-	parent = triangle[parent_row_count][parent_row_idx]
+	# If the node has a left child
+	if check_left_child(triangle, node_row_count, node_row_idx):
 
+		left_subtree_max = calc_max_path_recursive(triangle, memory_dict, node_row_count + 1, node_row_idx)
+
+	# If the node has a right child
+	if check_right_child(triangle, node_row_count, node_row_idx):
+
+		right_subtree_max = calc_max_path_recursive(triangle, memory_dict, node_row_count + 1, node_row_idx + 1)
+
+	node = triangle[node_row_count][node_row_idx]
+
+	# If the node has at least 1 child
 	if left_subtree_max or right_subtree_max:
+
+		val_to_return = None
 		
 		# If there are both childs, check which value from which subtree (left or right) is bigger
 		if left_subtree_max and right_subtree_max:
-			if left_subtree_max > right_subtree_max:
-				return parent + left_subtree_max
-			return parent + right_subtree_max
+			val_to_return = node + left_subtree_max if left_subtree_max > right_subtree_max else node + right_subtree_max
 
-		# If there is only one child, sum its value with the parent and return
+		# If there is only one child, sum its value with the node and return
 		else:
-			if left_subtree_max and not right_subtree_max:
-				return parent + left_subtree_max
-			return parent + right_subtree_max
+			val_to_return = node + left_subtree_max if left_subtree_max and not right_subtree_max else node + right_subtree_max
+
+		memory_dict[memory_key] = val_to_return
+		return val_to_return
 
 	# If no left or right child (a leaf), return its value
 	else:
-		return parent
+		return node
 
 
 # Read the filename from the command line (first command)
 triangle = read_triangle(sys.argv[1])
 
 # Calculate and print max path
-print(calc_max_path_recursive(triangle, 0, 0))
+print(calc_max_path_recursive(triangle, dict(), 0, 0))
